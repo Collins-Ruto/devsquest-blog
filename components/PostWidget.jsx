37e46fd38e38fd8d3cc582f12656778b7a454c60
collins-ruto <collinsruto48@gmail.com>
2022-11-03 15:04:27 +0300

    implimented postwidgets

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Link from "next/link";
-import { getRecentPosts } from "../services";
+import { getRecentPosts, getSimilarPosts } from "../services";
 
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
@@ -12,9 +12,28 @@ const PostWidget = ({ categories, slug }) => {
     } else {
       getRecentPosts().then((result) => setRelatedPosts(result));
     }
-  }, [third]);
+  }, [slug]);
 
-  return <div>PostWidget</div>;
+  console.log(relatedPosts)
+
+  return (
+  <div className="bg-white shadow-lg rounded-ld p-8 mb-8">
+    <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+      {slug? 'Related Posts': 'Recent Posts'}
+    </h3>
+    {relatedPosts.map((post) => (
+      <div key={post.title} className="flex items-center w-full mb-4">
+        <div className="w-16 flex-none">
+          <img src={post.featuredImage.url} alt={post.title} height='60px' width='60px' className="line-middle rounded-full"/>
+        </div>
+        <div className="flex-grow ml-4">
+          <p className="text-gray-500 font-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
+          <Link href={`/post/${post.slug}`}>{post.title}</Link>
+        </div>
+      </div>
+    ))}
+    </div>
+    )
 };
 
 export default PostWidget;

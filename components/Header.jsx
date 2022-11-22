fb37c93f95c10c6ef9d6d26fbcb9d564c0f35a62
collins-ruto <collinsruto48@gmail.com>
2022-11-22 18:37:32 +0300

    initial commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,40 +1,63 @@
-import React, {useContext, useState, useEffect} from 'react'
-
-import Link from 'next/link'
-import { getCategories } from '../services';
+import React, { useContext, useState, useEffect } from "react";
+import classNames from "classnames";
+import Link from "next/link";
+import { getCategories } from "../services";
 
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+  const [opened, setOpened] = useState(false);
 
-    const [categories, setCategories] = useState([]);
-
-    useEffect(() => {
-      getCategories().then((newCategories) => {
-        setCategories(newCategories);
-      });
-    }, []);
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
 
   return (
-    <div className='container mx-auto px-10 mb-8'>
-        <div className="border-b-gray-200 w-full inline-block border-blue-400 py-8">
-            <div className="md:float-left block">
-                <Link href="/">
-                    <span className="cursor-pointer font-bold text-4xl text-white">
-                        DevsQuest
-                    </span>
-                </Link>
-            </div>
-            <div className="hidden md:float-left md:contents">
-                {categories.map(category => (
-                    <Link key={category.slug} href={`/category/${category.slug}`}>
-                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
-                            {category.name}
-                        </span>
-                    </Link>
-                ))}
-            </div>
+    <div className=" mx-auto sticky top-0 z-40 bg-white px-10 mb-4">
+      <div className="border-b-gray-200 w-full flex items-center justify-between md:inline-block border-blue-400 py-3">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-pink-500 hover:text-pink-400 text-4xl">
+              DevsQuest
+            </span>
+          </Link>
         </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle ml-4 font-semibold text-pink-500 hover:text-pink-400 cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+        <div
+          className={classNames(`tham md:hidden tham-e-squeeze tham-w-8`, {
+            "tham-active": opened,
+          })}
+        >
+          <div className="tham-box">
+            <div className="tham-inner bg-pink-500" onClick={()=>{setOpened(!opened)}}/>
+          </div>
+        </div>
+      </div>
+      {opened && 
+      <div className="">
+        <div className="absolute left-0 w-[100%] md:w-80 p-4 h-screen opacity-50 bg-blend-darken bg-white" onClick={() => {setOpened(!opened)}}></div>
+        <div className="flex flex-col absolute right-0 w-[60%] md:w-80 p-4 h-screen opacity-100 bg-blend-darken bg-white">
+            <span className="font-semibold text-xl mb-3"> Categories</span>
+            {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`} onClick={() => {setOpened(!opened)}}>
+                <span className="mt-2 align-middle ml-2 font-semibold text-pink-500 hover:text-pink-400 cursor-pointer">
+                {category.name.substr(0, 18)}
+                </span>
+            </Link>
+            ))}
+      </div>
+      </div>}
     </div>
-  )
-}
+  );
+};
 
 export default Header
--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,20 @@ import {getPosts} from '../services'
 
 const Home = ({posts}) => {
   return (
-    <div className="container mx-auto p-2 lg:px-6 mb-8">
+    <div className="container mx-auto p-2 lg:px-4 mb-8">
       <Head>
         <title>DevsQuest Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-2">
         <div className="lg:col-span-9 col-span-1">
           <div className="flex flex-wrap gap-4">
               {posts.map((post, index) => (<PostCard post={post.node} key={index}/>))}
           </div>
         </div>
         <div className="lg:col-span-3 col-span-1">
-          <div className="lg:sticky relative top-8">
+          <div className="lg:sticky relative top-20">
             <PostWidget />
             <Categories /> 
           </div>
